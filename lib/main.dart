dec07bde94571485994199be0367912fae893cdc
Beren Erchamion <beren@houseofbeor.net>
2020-09-06 14:12:42 -0400

    Updating main page name.

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'O\'Reilly Course Home Page'),
+      home: MyHomePage(title: 'O\'Reilly Course Main Page'),
     );
   }
 }
