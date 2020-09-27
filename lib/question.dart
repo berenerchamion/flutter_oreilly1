9edf576575859630992c868c2b5b09881b739e0c
Beren Erchamion <beren@houseofbeor.net>
2020-09-26 22:02:59 -0400

    more styles

--- lib/question.dart
+++ lib/question.dart
@@ -9,7 +9,7 @@ class Question extends StatelessWidget {
   Widget build (BuildContext context) {
       return Container(
         width: double.infinity,
-        margin: EdgeInsets.all(2),
+        margin: const EdgeInsets.all(2),
         child: Text(questionText,
         style: TextStyle(
           fontSize: 28,
